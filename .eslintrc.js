99e120333ce61dd2d3a89e98757d4eeb43ef05b9
ernie-hao <pdphiro@gmail.com>
2018-08-25 18:33:27 -0400

    Remove startup files, first page

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": ['plugin:react/recommended'],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "react/jsx-uses-vars": [2],
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import 'typeface-roboto';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Homepage from 'containers/Homepage';
+
+const mainTheme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#f04d22'
+        },
+        secondary: {
+            main: '#e91e63',
+        },
+        text: {
+            primary: '#f04d22'
+        },
+    }
+});
+class App extends React.Component {
+    render() {
+        return (<Homepage />);
+    }
+}
+
+ReactDOM.render(
+    <Router>
+        <MuiThemeProvider theme={mainTheme}>
+            <Route component={ConnectedApp} />
+        </MuiThemeProvider>
+    </Router>
+    , document.getElementById('root'));
+
+export default App;
