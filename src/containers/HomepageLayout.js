215a56b8317e77ace42ffd60111baddfe0ab9683
ernie-hao <pdphiro@gmail.com>
2018-08-26 16:11:33 -0400

    home page update

--- src/containers/HomepageLayout.js
+++ src/containers/HomepageLayout.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { Grid, Typography } from '@material-ui/core';
 
+import backgroundImage from '../assets/homepage_cover_1440.jpg';
+
 const styles = {
     wrapper: {
-        height: '728px'
+        height: '780px',
+        backgroundImage: `url(${backgroundImage})`,
     },
     titleSpacing: {
         height: '300px'
+    },
+    text: {
+        color: '#ffffff'
+    },
+    name: {
+        color: '#2d80c4'
     }
 };
 
@@ -18,9 +27,15 @@ export class HomepageLayout extends Component {
                 </div>
                 <Grid
                     container
-                    justify='center'>
-                    <Typography variant='display4' item='true'>
-                        Ernie Hao
+                    direction='column'
+                    alignItems='center'
+                    >
+                     <Typography variant='title' item='true' style={styles.text}>
+                        <em>Hiya, I'm</em>
+              </Typography>
+                
+                    <Typography className="bg-light rounded pl-3 pr-3 mt-2" variant='display3' item='true' style={styles.name}>
+                        ernie hao
               </Typography>
                 </Grid>
                 <div style={styles.titleSpacing}>
